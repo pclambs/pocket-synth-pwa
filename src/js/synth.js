8d28943d83fa8c8e73ca620e7148744ca0b3ab92
deepwubs <pchucklambs@gmail.com>
2023-10-16 14:42:03 -0500

    added array for active playing notes to create proper mono synth playability

--- src/js/synth.js
+++ src/js/synth.js
@@ -14,14 +14,36 @@ export class PocketSynth {
         this.transport = Transport
         this.transport.bpm.value = 120
 
+        // init activeNotes array. looks for notes being actively held down
+        this.activeNotes = []
+
+        // keeps track of current note being played
+        this.currentlyPlayingNote = null
+
+        // init audioKeys
         this.audiokeys = new AudioKeys()
 
         this.audiokeys.down(note => {
+            // add new note to activeNotes array
+            this.activeNotes.push(note.note)
             this.triggerAttackByKey(note)
         })
 
         this.audiokeys.up(note => {
-            this.triggerRelease()
+            // find index of last released note and remove from array
+            const index = this.activeNotes.lastIndexOf(note.note)
+            if (index > -1) {
+                this.activeNotes.splice(index, 1)
+            }
+
+            if (this.activeNotes.length > 0) {
+                // fall back to previous note being held
+                this.triggerAttackByKey({note: this.activeNotes[this.activeNotes.length - 1]})
+            } else {
+                // trigger release when no active notes
+                this.triggerRelease()
+            }
+        
         })
 
         this.initUI()
@@ -30,19 +52,19 @@ export class PocketSynth {
     initUI() {
         document.getElementById('attack').addEventListener('input', (e) => {
             this.setAttack(e.target.value)
-        });
+        })
 
         document.getElementById('decay').addEventListener('input', (e) => {
             this.setDecay(e.target.value)
-        });
+        })
 
         document.getElementById('sustain').addEventListener('input', (e) => {
             this.setSustain(e.target.value)
-        });
+        })
 
         document.getElementById('release').addEventListener('input', (e) => {
             this.setRelease(e.target.value)
-        });
+        })
     }
 
     setAttack(value) {
@@ -70,10 +92,16 @@ export class PocketSynth {
 
     triggerAttackByKey(note) {
         const keyNote = this.midiToNote(note.note)
-        this.synth.triggerAttack(keyNote)
+        if (keyNote != this.currentlyPlayingNote) {
+            this.synth.triggerAttack(keyNote)
+            this.currentlyPlayingNote = keyNote
+        }
     }
 
     triggerRelease() {
-        this.synth.triggerRelease()
+        if (this.activeNotes.length === 0) {
+            this.synth.triggerRelease()
+            this.currentlyPlayingNote = null
+        }
     }
 }
